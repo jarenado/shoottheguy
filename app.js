7987333abb1796587b7dfeefe72d3c26a082524d
Jose Arenado <vidalstat@gmail.com>
2017-10-07 18:20:18 -0400

    fixed multiple event handling

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ function createEnemies(num) {
     pos = getPosition;
     battlefield.appendChild(enemy);
     attack(enemy, pos);
+    attachHandler(enemy);
+  }
+}
+
+function attachHandler(enemy) {
+  enemy.onclick = function(e) {
+    e.target.className += " poof";
   }
 }
 
@@ -19,26 +26,10 @@ function attack(enemy, pos) {
   console.log('pos', pos)
   pos = pos + 10;
   enemy.style.top = pos + "px"
-  /* var enemies = document.getElementsByClassName('enemy');*/
-  /* for (i = 1;i < enemies.length; i++) {*/
-  /* var enemy = enemies[i];*/
-  /* if (pos == 450) {*/
-  /* console.log('stop');*/
-  /* } else {*/
-  /* pos = pos + 10;*/
-  /* enemy.style.top = pos + "px";*/
-  /* console.log(enemy);*/
-  /* }*/
-  /* }*/
   setTimeout(function(){
     attack(enemy, pos)
   }, 500);
 }
-/* document.querySelector(".enemy").onclick = function(e, timer1){
- *   clearTimeout(timer1);
- *   var enemy = e.target;
- *   enemy.className += " poof"
- * }*/
+
 
 createEnemies(numOfEnemies);
-attack();
