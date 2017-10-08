dfb880e6e40ee88f6d7acdfe5841cf8e22f425ad
Jose Arenado <vidalstat@gmail.com>
2017-10-08 17:29:28 -0400

    added flex box

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-var numOfEnemies = 7,
+var numOfEnemies = 4,
     battlefield = document.querySelector(".app"),
     pos = 0,
-    score = 0;
+    score = 0,
+    speed = 100;
 
 function createEnemies(num) {
   for (i=0;i < num; i++) {
     var enemy = document.createElement("div");
     var getPosition = Math.floor((Math.random() * 100) + 1) * 3;
     enemy.className = "enemy enemy" + i; 
-    enemy.style.left = 120 * i + "px";
-    enemy.style.top = "-" + getPosition + "px";
+    enemy.style.top = 120 * i + "px";
+    enemy.style.left = "-" + getPosition + "px";
     pos = getPosition;
     battlefield.appendChild(enemy);
     attack(enemy, pos);
@@ -26,24 +27,27 @@ function updateScore() {
 
 }
 
-function killGuy() {
+function killGuy(guy) {
   //kill guy
+  
+  console.log(guy.offsetTop);
   //update score
 }
 
 function attachHandler(enemy) {
   enemy.onclick = function(e) {
     e.target.className += " poof";
+    killGuy(e.target);
     updateScore();
   }
 }
 
 function attack(enemy, pos) {
   pos = pos + 10;
-  enemy.style.top = pos + "px"
+  enemy.style.left = pos + "px"
   setTimeout(function(){
     attack(enemy, pos)
-  }, 500);
+  }, speed);
 }
 
 createEnemies(numOfEnemies);
