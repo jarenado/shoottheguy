e8d7db317bfc843268cd31f8255869a1ce3051b6
Jose Arenado <vidalstat@gmail.com>
2017-10-08 20:32:42 -0400

    added sass-loader (FINALLY!!!!!)

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,23 +5,32 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "bundle.js"
+    filename: "bundle.js",
+    publicPath: '/dist/'
   },
   module: {
     rules: [
       { // regular css files
         test: /\.css$/,
         exclude: /node_modules/,
-        use: 'css-loader'
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader?importLoaders=1',
+        }),
       },
 
       { // sass / scss loader for webpack
         test: /\.(sass|scss)$/,
         exclude: /node_modules/,
-        use: ['style-loader','css-loader',  'sass-loader']
+        use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
       }
     ]
   },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'dist/[name].index.css',
+      disable: false
+    })
+  ],
   watch: true
 };
 
